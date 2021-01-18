18101b5876f131c58a4ba1eab50cdcef2eeec546
dong uk <gusrn1255@naver.com>
2021-01-18 18:06:13 +0900

    https://api.themoviedb.org/3/movie/464052?api_key=8597e491ed6e80f0de12e349eb60ea6e&language=en-US
    데이터 샘플링 진행시 데이터 구성배열  ex [ ] , { } 확인 필요

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    //1. 데이터를 담을 공간 useState 함수사용
+    const [movie, setMovie] = useState({})
+    const [loading, setLoading] = useState(true)
+
+    //3. 네트워킹 함수 axios이용
+    const getData = async () => {
+        return (
+            await axios.get('https://api.themoviedb.org/3/movie/464052?api_key=8597e491ed6e80f0de12e349eb60ea6e&language=en-US')
+                // .then(movie => console.log(movie.data.genres)) 네트워킹 정보출력 확인
+                .then(movie => {
+                    setMovie(movie.data)
+                    setLoading(false)
+                })
+                .catch(err => console.log(err))
+
+        )
+    }
+
+
+
+
+
+    //2. 자동실행 함수 useEffect 함수 사용
+    useEffect(() => {
+        getData()
+    },{})
+
+    return (
+        <>
+            {loading ?
+                <div>
+                    <h1>
+                        loading ...
+                    </h1>
+                </div>
+            :  (
+                        <div>
+
+                                <h1>{movie.original_title}</h1>
+                                <h2>{movie.overview}</h2>
+                                {movie.production_companies.map(production_companies => (
+                                    <h3>{production_companies.name}</h3>
+                                        ))}
+
+                                    </div>
+
+
+                )}
+            }
+
+        </>
+
+    );
+};
 
 export default App;
